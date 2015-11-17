007d8f7da8c4e8c1f63e83dc1b2c2802e1261975
Ben Lubar <ben.lubar@gmail.com>
2015-11-17 11:08:25 -0600

    problem 5

---
+++ 0005/main.go
@@ -0,0 +1,24 @@
+// Smallest multiple
+// Problem 5
+// 2520 is the smallest number that can be divided by each of the numbers from
+// 1 to 10 without any remainder.
+//
+// What is the smallest positive number that is evenly divisible by all of the
+// numbers from 1 to 20?
+package main
+
+import "fmt"
+
+func main() {
+search:
+	for i := 1; ; i++ {
+		for j := 1; j <= 20; j++ {
+			if i%j != 0 {
+				continue search
+			}
+		}
+
+		fmt.Println(i)
+		return
+	}
+}
