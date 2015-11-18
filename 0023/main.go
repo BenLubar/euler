09754da06113c4f2393c85d39b836801bf01b4df
Ben Lubar <ben.lubar@gmail.com>
2015-11-17 18:14:59 -0600

    problem 23

---
+++ 0023/main.go
@@ -0,0 +1,67 @@
+// Non-abundant sums
+// Problem 23
+// A perfect number is a number for which the sum of its proper divisors is
+// exactly equal to the number. For example, the sum of the proper divisors of
+// 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+//
+// A number n is called deficient if the sum of its proper divisors is less than
+// n and it is called abundant if this sum exceeds n.
+//
+// As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest
+// number that can be written as the sum of two abundant numbers is 24. By
+// mathematical analysis, it can be shown that all integers greater than
+// 28123 can be written as the sum of two abundant numbers. However, this upper
+// limit cannot be reduced any further by analysis even though it is known that
+// the greatest number that cannot be expressed as the sum of two abundant
+// numbers is less than this limit.
+//
+// Find the sum of all the positive integers which cannot be written as the sum
+// of two abundant numbers.
+package main
+
+import (
+	"fmt"
+
+	"github.com/BenLubar/memoize"
+)
+
+func main() {
+	var sum int
+
+	for i := 1; i <= 28123; i++ {
+		if nonAbundantSum(i) {
+			sum += i
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func nonAbundantSum(n int) bool {
+	for i := 12; i < n; i++ {
+		if !isAbundant(i) {
+			continue
+		}
+
+		if isAbundant(n - i) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isAbundant(n int) bool {
+	return d(n) > n
+}
+
+var d = memoize.Memoize(func(n int) int {
+	var sum int
+	for i := 1; i < n; i++ {
+		if n%i == 0 {
+			sum += i
+		}
+	}
+
+	return sum
+}).(func(n int) int)
